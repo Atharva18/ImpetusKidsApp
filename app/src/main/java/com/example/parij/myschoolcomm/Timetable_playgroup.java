3d8d11e8dc18ebda09674d69dea6de578e07551f
Atharva98 <atharva18karanje@gmail.com>
2018-01-14 00:44:33 +0530

    TimeTable Added

---
+++ app/src/main/java/com/example/parij/myschoolcomm/Timetable_playgroup.java
@@ -0,0 +1,38 @@
+package com.example.parij.myschoolcomm;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+public class Timetable_playgroup extends AppCompatActivity {
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+
+            case android.R.id.home:
+                this.finish();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_timetable_playgroup);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        getSupportActionBar().setTitle("Time-Table");
+
+        toolbar.setTitleTextColor(0xFFFFFFFF);
+
+    }
+}
