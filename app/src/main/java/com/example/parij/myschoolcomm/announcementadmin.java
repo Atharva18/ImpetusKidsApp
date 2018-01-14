d26361dcebfe7ecef0e2177be2ff691d0ae13454
Chandan Singh <sc231997@gmail.com>
2018-01-14 06:15:51 +0530

    announcement in admin done.

--- app/src/main/java/com/example/parij/myschoolcomm/announcementadmin.java
+++ app/src/main/java/com/example/parij/myschoolcomm/announcementadmin.java
@@ -1,25 +1,38 @@
 package com.example.parij.myschoolcomm;
 
-import android.os.SystemClock;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class announcementadmin extends AppCompatActivity {
-
+    ListView listViewAnnouncement;
+    Spinner spinnerFilter;
+    ArrayAdapter arrayAdapter, arrayAdapterSpinner;
     EditText announcement;
     Button sendbtn;
     Broadcast broadcast;
     FirebaseDatabase firebaseDatabase;
     DatabaseReference databaseReference;
+    ArrayList<Broadcast> arrayListBroadcast, filterAnnouncementArrayList;
+    ArrayList<String> arrayListMsg, spinnerArrayList;
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -51,7 +64,17 @@ public class announcementadmin extends AppCompatActivity {
         //  toolbar.setNavigationIcon(R.drawable.childprofbar);
         toolbar.setTitleTextColor(0xFFFFFFFF);
 
+        spinnerFilter.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                filter(position);
+            }
 
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 
 
         sendbtn.setOnClickListener(new View.OnClickListener() {
@@ -68,13 +91,79 @@ public class announcementadmin extends AppCompatActivity {
             }
         });
 
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot ds : dataSnapshot.getChildren()) {
+                    arrayListBroadcast.add(ds.getValue(Broadcast.class));
+                }
+                sort(arrayListBroadcast);
+                filter(spinnerFilter.getSelectedItemPosition());
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
     }
 
     private void main_initiate() {
-
+        listViewAnnouncement = (ListView) findViewById(R.id.listViewAnnouncementAdmin);
         announcement=(EditText)findViewById(R.id.announcemnttext);
         sendbtn=(Button)findViewById(R.id.sendbtn);
         firebaseDatabase = FirebaseDatabase.getInstance();
+        spinnerFilter = (Spinner) findViewById(R.id.spinnerFilterAnnouncementAdmin);
+        arrayListBroadcast = new ArrayList<>();
+        arrayListMsg = new ArrayList<>();
+        spinnerArrayList = new ArrayList<>();
+        spinnerArrayList.add("None");
+        spinnerArrayList.add("5 Days");
+        spinnerArrayList.add("10 Days");
+        spinnerArrayList.add("15 Days");
+        arrayAdapterSpinner = new ArrayAdapter(announcementadmin.this, android.R.layout.simple_spinner_dropdown_item, spinnerArrayList);
+        spinnerFilter.setAdapter(arrayAdapterSpinner);
+
         databaseReference = firebaseDatabase.getReference().child("broadcast");
+    }
+
+    void sort(ArrayList<Broadcast> arrayListBroadcast) {
+        for (int i = 0; i < arrayListBroadcast.size(); i++) {
+            for (int j = 0; j < arrayListBroadcast.size() - 1; j++) {
+                if (arrayListBroadcast.get(j).getTimestamp() < arrayListBroadcast.get(j + 1).getTimestamp()) {
+                    Broadcast temp = arrayListBroadcast.get(j);
+                    arrayListBroadcast.set(j, arrayListBroadcast.get(j + 1));
+                    arrayListBroadcast.set(j + 1, temp);
+                }
+            }
+        }
+        for (int i = 0; i < arrayListBroadcast.size(); i++) {
+            arrayListMsg.add("Message : " + arrayListBroadcast.get(i).getMsg());
+        }
+    }
+
+    void filter(int position) {
+        filterAnnouncementArrayList = new ArrayList<>();
+        arrayListMsg = new ArrayList<>();
+        Log.d("position", "" + position);
+        if (position == 0) {
+            filterAnnouncementArrayList = arrayListBroadcast;
+        } else {
+            int day = position * 5;
+            long currentTimeStamp = System.currentTimeMillis();
+            long filterTimeStamp = day * 86400000;
+            for (int i = 0; i < arrayListBroadcast.size(); i++) {
+                Log.d("something", (currentTimeStamp - arrayListBroadcast.get(i).getTimestamp()) + "");
+                if (currentTimeStamp - arrayListBroadcast.get(i).getTimestamp() < filterTimeStamp) {
+                    filterAnnouncementArrayList.add(arrayListBroadcast.get(i));
+                }
+            }
+        }
+        for (int i = 0; i < filterAnnouncementArrayList.size(); i++) {
+            arrayListMsg.add("Message : " + arrayListBroadcast.get(i).getMsg());
+        }
+        arrayAdapter = new ArrayAdapter(announcementadmin.this, android.R.layout.simple_list_item_1, arrayListMsg);
+        listViewAnnouncement.setAdapter(arrayAdapter);
     }
 }
