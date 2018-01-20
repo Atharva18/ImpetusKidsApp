7ce790a22ac0ff1166b564555548ea82fe13e525
Ankush-Pathak <ankush.pathak151196@gmail.com>
2018-01-20 22:13:27 +0530

    Removed unnecessary menus

--- app/src/main/java/com/example/parij/myschoolcomm/adminparent.java
+++ app/src/main/java/com/example/parij/myschoolcomm/adminparent.java
@@ -76,12 +76,7 @@ public class adminparent extends AppCompatActivity {
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_adminparent, menu);
-        return true;
-    }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
